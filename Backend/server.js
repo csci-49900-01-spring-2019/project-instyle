2aefda8b1011f9ebf6df2b551f86d4d5c5be70e6
Lili Chen <lilichen0326@gmail.com>
2019-02-24 18:02:19 -0500

    Add Login and Register

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,71 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+var firebase = require("firebase");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var config = {
+    apiKey: "AIzaSyBfkeTA-3ivv_EUOi6vTj5UP4530moiec4",
+    authDomain: "instyle-5f93a.firebaseapp.com",
+    databaseURL: "https://instyle-5f93a.firebaseio.com",
+    projectId: "instyle-5f93a",
+    storageBucket: "instyle-5f93a.appspot.com",
+    messagingSenderId: "620412495745"
+  };
+  
+firebase.initializeApp(config);
 
 // create a GET route
 app.get('*', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.post('/auth', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   const promise = firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
+	.then(function(firebaseUser) {
+		response = {
+			isAuth: true,
+			uid: firebaseUser.user.uid
+		};
+		res.end(JSON.stringify(response));
+	})
+	.catch(function(error) {
+		response = {
+			isAuth: false,
+			message: error.message
+		};
+		res.end(JSON.stringify(response));
+		});
+})
+
+app.post('/register', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   const promise = firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
+	.then(function(firebaseUser) {
+		response = {
+			isAuth: true,
+			uid: firebaseUser.user.uid
+		};
+		res.end(JSON.stringify(response));
+	})
+	.catch(function(error) {
+		response = {
+			isAuth: false,
+			message: error.message
+		};
+		res.end(JSON.stringify(response));
+		});
+})
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- Frontend/client/src/Login.js
+++ Frontend/client/src/Login.js
@@ -6,32 +6,116 @@ class Login extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+			uid: ''
         }
+		this.handleSignIn = this.handleSignIn.bind(this);
+		this.handleSignOut = this.handleSignOut.bind(this);
+		this.setAuth = this.setAuth.bind(this);
+		this.handleRegister = this.handleRegister.bind(this);
     }
     componentDidMount() {
         //axios request
     }
+	
+	setAuth(res){
+		if(!res.isAuth)
+		{
+			document.getElementById("password").value = "";
+			document.getElementById("message").value = res.message;
+		}
+		else
+		{
+			this.setState({
+				uid: res.uid
+			})
+			document.getElementById("signIn").disabled = true;
+			document.getElementById("register").disabled = true;
+			document.getElementById("signOut").disabled = false;
+			document.getElementById("email").value = "";
+			document.getElementById("password").value = "";
+			document.getElementById("message").value = "Welcome, uid:" + res.uid;
+		}
+	}
+	
+	handleSignIn(event) {
+		event.preventDefault();
+		this.callSignInAPI().then(res => this.setAuth(res)).catch(function(error) {
+				console.log(error);
+			});
+	}
+	
+	handleRegister(event) {
+		event.preventDefault();
+		this.callRegisterAPI().then(res => this.setAuth(res)).catch(function(error) {
+				console.log(error);
+			});
+	}
+	
+	handleSignOut(event) {
+		event.preventDefault();
+		this.setState({
+				uid: ""
+			})
+		document.getElementById("signIn").disabled = false;
+		document.getElementById("register").disabled = false;
+		document.getElementById("signOut").disabled = true;
+		document.getElementById("email").value = "";
+		document.getElementById("password").value = "";
+		document.getElementById("message").value = "Signed Out";
+	}
+	
+	// Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callSignInAPI = async () => {
+        const response = await fetch('/auth', {
+			method: 'post',
+			headers: {'Content-Type':'application/json'},
+			body: JSON.stringify({
+				email: document.getElementById("email").value,
+				password: document.getElementById("password").value
+			})
+		})
+        const body = await response.json();
+		if (response.status !== 200) {
+			throw Error(body.message);
+		}
+        return body;
+    };
+	
+	callRegisterAPI = async () => {
+        const response = await fetch('/Register', {
+			method: 'post',
+			headers: {'Content-Type':'application/json'},
+			body: JSON.stringify({
+				email: document.getElementById("email").value,
+				password: document.getElementById("password").value
+			})
+		})
+        const body = await response.json();
+		if (response.status !== 200) {
+			throw Error(body.message);
+		}
+        return body;
+    };
 
     render(){
         return(
             <div>
                 <form className="signUpForm">
                     <div>
-                        <input id="email" type="text" placeholder="Email" />
+                        <input id="email" type="text" placeholder="Email"/>
                     </div>
                     <div>
-                        <input id="password" type="password" placeholder="Password" />
-                    </div>
-                    <br/>
-                    <div>
-                        <button id="signIn">Sign In</button>
-                        <button id="register">Register</button>
-                        <button id="signOut" >Sign Out</button>
+                        <input id="password" type="password" placeholder="Password"/>
                     </div>
                     <br/>
                     <div>
-                        <input id="message" type="text"  disabled='true' size="30"/>
+                        <button id="signIn" onClick={this.handleSignIn} disabled ={false}>Sign In</button>
+                        <button id="register" onClick={this.handleRegister} disabled ={false}>Register</button>
+                        <button id="signOut" onClick={this.handleSignOut} disabled ={true}>Sign Out</button>
+                    </div>
+                    <br/>
+                    <div>
+                        <input id="message" type="text" value={this.state.message} disabled='1' size="30"/>
                     </div>
                 </form>
             </div>
