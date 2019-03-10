7e03c8da479102d8948558efed88606101c3001a
Jasmeet <jasmeetnrng@gmail.com>
2019-03-10 17:09:07 -0400

    auth working in signup and login component

--- Frontend/client/src/Login.js
+++ Frontend/client/src/Login.js
@@ -7,52 +7,82 @@ class Login extends Component{
 		super(props);
 		this.state = {
 			uid:'',
-			useremail:"",
+			email:"",
 			password:"",
-			auth:false
+			isAuth:false,
+			message:""
 		}
 
+
 	}
-	handleSubmit() {
+	handleChange(evt){
+		this.setState({[evt.target.name]:evt.target.value});
+	}
+
+	handleSubmit(evt) {
+		//console.log("email: ", this.state.email)
+		evt.preventDefault()
+
+		console.log("Signing In")
+
 		axios.post("/auth",{
-			method:'post',
-			headers:{'Content-Type':'application/json'},
-			body:JSON.stringify({
-				email:this.state.useremail,
-				password:this.state.password
-			})
-		}
-		.then(function(response){
-			console.log(response)
-			}).catch(function(error){
-				console.log(error);
-			})
-		)
+			email:this.state.email,
+			password:this.state.password
+		})
+			.then(response => {
+				console.log(response);
+				if(response.data.isAuth) {
+					this.setState(
+						{
+							isAuth: response.data.isAuth,
+							uid: response.data.uid,
+							message: "Welcome " + this.state.email
+						}
+					)
+				} else {
+					this.setState(
+						{
+							isAuth: response.data.isAuth,
+							uid: null,
+							message: response.data.message
+						}
+					)
+				}
+				console.log("isAuth: ", this.state.isAuth)
+				console.log("uid: ", this.state.uid)
+			}).catch(function (error) {
+			console.log("Authorization failed: "+ error.message);
+		})
+
 	}
 
-    render(){
-        return(
-            <div>
-                <form className="signUpForm">
-                    <div>
-                        <input id="email" type="text" placeholder="Email"/>
-                    </div>
-                    <div>
-                        <input id="password" type="password" placeholder="Password"/>
-                    </div>
-                    <br/>
-                    <div>
-                        <button id="signIn" onClick={this.handleSubmit} disabled ={false}>Sign In</button>
-                        <button id="signOut" onClick={this.handleSignOut} disabled ={true}>Sign Out</button>
-                    </div>
-                    <br/>
-                    <div>
-                        <input id="message" type="text" value={this.state.message} disabled='1' size="30"/>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+	handleSignOut(){
+	// 	axios.get("/logout")
+	}
+
+	render(){
+		return(
+			<div>
+				<form className="signUpForm">
+					<div>
+						<input name="email" type="text"  value={this.state.email} placeholder="Email" onChange={this.handleChange.bind(this)}/>
+					</div>
+					<div>
+						<input name="password" autoComplete="new-password" type="password" value={this.state.password} placeholder="Password" onChange={this.handleChange.bind(this)}/>
+					</div>
+					<br/>
+					<div>
+						<button id="signIn" onClick={this.handleSubmit.bind(this)} >Sign In</button>
+						<button id="signOut" onClick={this.handleSignOut.bind(this)} >Sign Out</button>
+					</div>
+					<br/>
+					<div>
+						<input id="message" readOnly={true} value={this.state.message}  size="30"/>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Login;
--- Frontend/client/src/Signup.js
+++ Frontend/client/src/Signup.js
@@ -2,27 +2,77 @@ import React, { Component } from "react";
 import "./Styles/Signup.css"
 import axios from "axios";
 
-class Signup extends Component{
 
+class Signup extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            uid:'',
+            email:"",
+            password:"",
+            isAuth:false,
+            message:""
+        }
+       // this.handleChange = this.handleChange.bind(this);
+        //this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    handleChange(evt){
+        this.setState({[evt.target.name]:evt.target.value});
+    }
+
+    handleSubmit(evt) {
+        //console.log("email: ", this.state.email)
+        evt.preventDefault()
+        console.log("Signing Up")
+        axios.post("/register",{
+            email:this.state.email,
+            password:this.state.password
+        })
+            .then(response => {
+                console.log(response);
+                if(response.data.isAuth) {
+                    this.setState(
+                        {
+                            isAuth: response.data.isAuth,
+                            uid: response.data.uid,
+                            message: "Welcome " + this.state.email
+                        }
+                    )
+                } else {
+                    this.setState(
+                        {
+                            isAuth: response.data.isAuth,
+                            uid: null,
+                            message: response.data.message
+                        }
+                    )
+                }
+                console.log("isAuth: ", this.state.isAuth)
+                console.log("uid: ", this.state.uid)
+            }).catch(function (error) {
+            console.log("Authorization failed: "+ error.message);
+        })
+    }
    render(){
         return(
             <div>
                 <form className="registerForm">
                     <div className="submission">
-                        <label for="email">Email</label>
-                        <input className="inputRegister" id="email" type="text"/>
+                        <label htmlFor="email">Email</label>
+                        <input className="inputRegister" autoComplete="username email" name="email" type="text" value={this.state.email} placeholder="Email" onChange={this.handleChange.bind(this)}/>
                     </div>
                     <div className="submission">
-                        <label for="password">Password</label>
-                        <input className="inputRegister" id="password" type="password"/>
+                        <label htmlFor="password">Password</label>
+                        <input className="inputRegister" autoComplete="new-password" name="password" type="password" value={this.state.password} placeholder="Password" onChange={this.handleChange.bind(this)}/>
                     </div>
                     <br/>
                      <div className="registerButton">
-                        <button className="submitButton" id="register" onClick={this.handleRegister} disabled ={false}>Register</button>
+                        <button className="submitButton" onClick={this.handleSubmit.bind(this)}>Register</button>
                     </div>
                     <br/>
                     <div>
-                        <input className="message" id="message" type="text" disabled='1' size="30"/>
+                        <input className="message" type="text" readOnly={true} value={this.state.message} size="30"/>
                     </div>
                 </form>
             </div>
