6aacb49d1d5dd56d2d58fdeaf7b16bb5daca0444
Matthew Li <matthewli2491@gmail.com>
2019-04-07 23:05:08 -0400

    login and register and storing token

--- Mobile/InStyle/InStyle/Controller/LoginViewController.swift
+++ Mobile/InStyle/InStyle/Controller/LoginViewController.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
 class LoginViewController: UIViewController {
     
@@ -21,7 +23,33 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginPressed(_ sender: Any) {
-        print("Loggin in ...")
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (AuthDataResult, Error) in
+            if Error != nil {
+                print(Error!)
+            }
+            
+            else {
+                print("Logged in successfully")
+                AuthDataResult!.user.getIDTokenResult(forcingRefresh: true, completion: {
+                    (AuthTokenResult, Error) in
+                    if Error != nil {
+                        print(Error!)
+                    }
+                    
+                    else {
+                        let token = AuthTokenResult!.token
+                        let saveSuccessful: Bool = KeychainWrapper.standard.set(token, forKey: "accessToken")
+                        print("Save was successful: \(saveSuccessful)")
+//                        let retrievedToken: String? = KeychainWrapper.standard.string(forKey: "accessToken")
+//                        print("Retrieved Token: \(retrievedToken!)")
+                    }
+                })
+                
+                
+                self.performSegue(withIdentifier: "goToTabViewController", sender: self)
+            }
+        }
     }
     
 }
--- Mobile/InStyle/InStyle/Controller/RegisterViewController.swift
+++ Mobile/InStyle/InStyle/Controller/RegisterViewController.swift
@@ -8,14 +8,19 @@
 
 import Foundation
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
 class RegisterViewController: UIViewController {
     
-    @IBOutlet weak var firstNameTextField: UITextField!
-    @IBOutlet weak var lastNameTextField: UITextField!
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet var firstName: UITextField!
+    @IBOutlet var lastName: UITextField!
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,39 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func registerPressed(_ sender: Any) {
+        
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) {
+            (AuthDataResult, error) in
+            if error != nil {
+                print(error!)
+            }
+            else {
+                print("Registration Successful")
+                
+                AuthDataResult!.user.getIDTokenResult(forcingRefresh: true, completion: {
+                    (AuthTokenResult, error) in
+                    if error != nil {
+                        print(error!)
+                    }
+                    else {
+                        let token = AuthTokenResult!.token
+                        let saveSuccessful: Bool = KeychainWrapper.standard.set(token, forKey: "accessToken")
+                        print("Save was successful: \(saveSuccessful)")
+                        let db = Firestore.firestore()
+                        db.collection("users").document(AuthDataResult!.user.uid).setData([
+                            "email": self.email.text!,
+                            "first_name": self.firstName.text!,
+                            "last_name": self.lastName.text!,
+                            "user_name": self.userName.text!,
+                            "uid": AuthDataResult!.user.uid
+                            ])
+                    }
+                })
+                
+                
+                self.performSegue(withIdentifier: "goToTabViewController", sender: self)
+            }
+        }
     }
     
 }
